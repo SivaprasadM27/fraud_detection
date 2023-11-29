c7df4f071a6f988fe812b811bb4813e4f8c1cb2e
SivaprasadM27 <sivaprasad.coding@gmail.com>
2023-11-29 21:10:25 +0530

    model 2 is final

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,16 @@
 print ("adding all ml related code here")
+
+
+def model1():
+    print("add code model 1")
+
+
+def model2():
+    print("add code model q")
+
+
+def model3():
+    print("add code model 3")
+
+
+print("model 2 is best")
